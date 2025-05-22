5352b92c6c306c442401770f43289dde5b5213a1
JULIEN-85 <julienjudo9@gmail.com>
2025-05-22 10:57:05 +0200

    maj

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e08e63ec3616169a"; }
+/******/ 		__webpack_require__.h = function() { return "6a81eb6d0c17659e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: true
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" }
+        ],
+      },
+    ]
   },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  }
+  // Ajout de la configuration du serveur
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  // Désactiver le strict mode pour le développement
+  reactStrictMode: false,
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
